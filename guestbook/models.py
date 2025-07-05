0c6539cdd286fa427766f5d3446b883c6e2eae28
Arnat Asanov <asanovarnat2209@gmail.com>
2025-07-05 13:00:04 +0600

    add superuser, guestmodel & migrations (without 5 examples)

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class GuestEntry(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано'),
+    ]
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='active'
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
